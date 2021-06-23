d2bf5794609728044569ceffe436461639689973
Joe Osborne <josephbwo@hotmail.com>
2021-06-23 16:39:42 +0100

    exercise-1: Add Button to expand input form for user to add there own items to list, add function to take user input and add to list, then refresh and display growing list

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from werkzeug.utils import redirect
 from todo_app.data import session_items
 from todo_app.flask_config import Config
 
@@ -9,11 +11,16 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    #si.get_items()
     items=session_items.get_items()
-    print (items)
     return render_template('index.html',list=items)
 
+@app.route('/add', methods=['POST', 'GET'])
+def add_item():
+    if request.method == 'POST':
+        user_item=request.form.get('item_name')
+        if not user_item == "":
+            session_items.add_item(user_item)
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run()
