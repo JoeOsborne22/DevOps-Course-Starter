f09349a2c5a09f6fec49bf24787f402bdd151db3
Joe Osborne <josephbwo@hotmail.com>
2021-06-23 18:09:08 +0100

    exercise-1: enhance index.html to include form/buttons to mark items as complete and remove items from todo list, add function to mark items as completed, add function stub to remove items from todo list

--- todo_app/app.py
+++ todo_app/app.py
@@ -22,5 +22,33 @@ def add_item():
             session_items.add_item(user_item)
     return redirect("/")
 
+@app.route('/mark', methods=['POST', 'GET'])
+def alter_item():
+    if request.method == 'POST':
+        try:
+            user_id=int(request.form.get('item_id'))
+        except:
+            user_id=0
+        if (user_id > 0):
+            item=session_items.get_item(user_id)
+            item['status']='Completed'
+            session_items.save_item(item)
+    return redirect("/")
+
+@app.route('/rem', methods=['POST', 'GET'])
+def remove_item():
+    print("Remove item")
+    if request.method == 'POST':
+        try:
+            user_id=int(request.form.get('item_id'))
+        except:
+            user_id=0
+        if (user_id > 0):
+            item=session_items.get_item(user_id)
+        print (item)
+        #TODO Need function to remove item
+    return redirect("/")
+
+
 if __name__ == '__main__':
     app.run()
