bad5a062c19d80efa2724d32388ac8d62ae8ea1c
Joe Osborne <josephbwo@hotmail.com>
2021-07-01 12:52:59 +0100

    exercise-1: Apply changes/reccomendations from PR review

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,8 +1,5 @@
 from operator import itemgetter
-from flask import Flask
-from flask import render_template
-from flask import request
-from werkzeug.utils import redirect
+from flask import Flask, render_template, request, redirect
 from todo_app.data import session_items
 from todo_app.flask_config import Config
 
@@ -16,42 +13,41 @@ def index():
     return render_template('index.html',list=items)
 
 
-@app.route('/add', methods=['POST', 'GET'])
+@app.route('/add', methods=['POST'])
 def add_item():
-    if request.method == 'POST':
-        user_item=request.form.get('item_name')
-        if not user_item == "":
-            session_items.add_item(user_item)
+    
+    user_item=request.form.get('item_name')
+    if not user_item == "":
+        session_items.add_item(user_item)
     return redirect("/")
 
-@app.route('/mark', methods=['POST', 'GET'])
+@app.route('/mark', methods=['POST'])
 def alter_item():
-    if request.method == 'POST':
-        try:
-            user_id=int(request.form.get('item_id'))
-        except:
-            user_id=0
-        if (user_id > 0):
-            item=session_items.get_item(str(user_id))
-            if not item == None:
-                item['status']='Completed'
-                session_items.save_item(item)
+    try:
+        user_id=int(request.form.get('item_id'))
+    except:
+        user_id=0
+    
+    item=session_items.get_item(str(user_id))
+    if not item == None:
+        item['status']='Completed'
+        session_items.save_item(item)
     return redirect("/")
 
-@app.route('/rem', methods=['POST', 'GET'])
+@app.route('/rem', methods=['POST'])
 def remove_item():
-    if request.method == 'POST':
-        try:
-            user_id=int(request.form.get('item_id'))
-        except:
-            user_id=0
-        if (user_id > 0):
-            item=session_items.get_item(user_id)
-            session_items.remove_item(item)
-        newItems=session_items.get_items()
-        for item in newItems:
-            item['id']=newItems.index(item)+1
-            session_items.save_item(item)
+    
+    try:
+        user_id=int(request.form.get('item_id'))
+    except:
+        user_id=0
+    
+    item=session_items.get_item(user_id)
+    session_items.remove_item(item)
+    newItems=session_items.get_items()
+    #for item in newItems:
+    #    item['id']=newItems.index(item)
+    #    session_items.save_item(item)
     return redirect("/")
 
 
